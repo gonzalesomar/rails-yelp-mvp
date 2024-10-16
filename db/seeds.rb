a22c57e2a543bedef04b66fa210984f36daa1844
gonzalesomar <gonzales.omar@pucp.edu.pe>
2024-10-15 19:32:32 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+Restaurant.create(name: "Epicure", address: "75008 Paris", phone_number: "0123456789", category: "french")
+Restaurant.create(name: "Café Tortoni", address: "Av. de Mayo 825, Buenos Aires", phone_number: "011-4342-4328", category: "japanese")
+Restaurant.create(name: "El Cuartito", address: "Talcahuano 937, Buenos Aires", phone_number: "011-4816-1758", category: "italian")
+Restaurant.create(name: "Bar El Federal", address: "Carlos Calvo 599, Buenos Aires", phone_number: "011-4300-4313", category: "belgian")
+Restaurant.create(name: "El Gaucho Parrilla", address: "Av. Corrientes 1234, Buenos Aires", phone_number: "011-4567-8901", category: "japanese")
+
+puts "Finished!"
